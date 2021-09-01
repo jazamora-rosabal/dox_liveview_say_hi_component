bc43748c723506e26a20fbbe851cdd4274de62dd
jazamora <julio.zamora@dox.cl>
2021-09-01 17:35:40 -0400

    init

--- mix.exs
+++ mix.exs
@@ -20,10 +20,6 @@ defmodule SayHiComponent.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
-
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
