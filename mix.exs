df7066c762d30576d98e86ce2734814fc2114057
jazamora <julio.zamora@dox.cl>
2021-09-01 17:36:32 -0400

    init

--- mix.exs
+++ mix.exs
@@ -30,15 +30,4 @@ defmodule SayHiComponent.MixProject do
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
-  defp aliases do
-    [
-      setup: ["deps.get", "cmd npm install --prefix assets"]
-    ]
-  end
 end
